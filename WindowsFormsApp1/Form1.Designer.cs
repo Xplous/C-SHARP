a1c55601d2b8631db4cddb840df75b7b5d7cf3a8
Xplous <nick.filippoff2016@yandex.ru>
2024-04-06 18:51:19 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Форма
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Форма
+            // 
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Font = new System.Drawing.Font("Arial", 12F);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Форма";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp1
+{
+    public partial class Форма : Form
+    {
+        public Форма()
+        {
+            InitializeComponent();
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            if (e.Button == MouseButtons.Left) {
+                string s = e.X.ToString();
+                g.DrawString(s, new Font("Times New Roman", 24),
+                new SolidBrush(Color.White), new Point(e.X, e.Y));
+            } 
+            else
+            {
+                g.Clear(Color.White);
+                MessageBox.Show("Поле было очищено","Сообщение");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
